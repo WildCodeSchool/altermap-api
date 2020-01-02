28d717d4d6e1ffe73c3ef92669841778527e3ddb
TanguyDuponchelle <tanguy.duponchelle@hotmail.fr>
2020-01-02 10:10:34 +0100

    new backlocal

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 4000;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -38,6 +40,20 @@ app.post('/api/v1/construction-sites', (req, res) => {
     },
   );
 });
+
+app.put('api/v1/construction-sites/:id', (req, res) => {
+  pool.query('UPDATE construction-sites SET $1 WHERE id=?',
+    [formData, idConstructionSites],
+    err => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Erreur lors de la modification d'un composant");
+      } else {
+        res.sendStatus(200);
+      }
+    }
+  )
+}
 
 app.listen(port, (err) => {
   if (err) {
