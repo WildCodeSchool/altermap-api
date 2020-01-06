ecdeae286466208cc35a2277a77c07e0175049f2
sergio Prieto <saprieto5@gmail.com>
2020-01-06 10:52:42 +0100

    Add Sequelize first table migration

---
+++ server/migrations/20200106093402-create-construction-site.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Construction-sites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      coords: {
+        allowNull: false,
+        type: Sequelize.DECIMAL,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Construction-sites');
+  }
+};
