fa183ef908ab85316f031fe117f9e63d26aafb3b
sergio Prieto <saprieto5@gmail.com>
2020-01-06 13:22:26 +0100

    Add get and put sequelize method, needs correction into put method, coords format

--- server.js
+++ server.js
@@ -1,33 +1,29 @@
 require('dotenv').config();
 
-const { Pool } = require('pg');
 const express = require('express');
 
 const app = express();
 const port = 4000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.PORTPG,
-});
+const { Construction_site } = require('./server/models');
 
-app.get('/api/v1/construction-sites', (req, res) => {
-  pool.query('SELECT * from construction_sites', (err, results) => {
+app.get('/api/v1/construction-sites', async (req, res) => {
+/*   pool.query('SELECT * from construction_sites', (err, results) => {
     if (err) {
       return res.status(500).send(err);
     }
     return res.send(results.rows);
-  });
+  }); */
+  const sites = await Construction_site.findAll();
+  res.send(sites);
 });
 
-app.post('/api/v1/construction-sites', (req, res) => {
+app.post('/api/v1/construction-sites', async (req, res) => {
   const { name, coords } = req.body;
-  pool.query(
+  /* pool.query(
     'INSERT INTO construction_sites (name,coords) VALUES ($1, $2)',
     [name, coords],
     (err, results) => {
@@ -36,7 +32,9 @@ app.post('/api/v1/construction-sites', (req, res) => {
       }
       return res.send(results);
     },
-  );
+  ); */
+  const site = await Construction_site.create({ name, coords });
+  res.send(site);
 });
 
 app.listen(port, (err) => {
