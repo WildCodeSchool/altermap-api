310bfc84c2f373879499cc49e3fd76a647936c6a
sergio Prieto <saprieto5@gmail.com>
2020-01-06 17:05:04 +0100

    Add put and delete sequelize-express route

--- server.js
+++ server.js
@@ -11,30 +11,28 @@ app.use(express.urlencoded({ extended: true }));
 const { Construction_site } = require('./server/models');
 
 app.get('/api/v1/construction-sites', async (req, res) => {
-/*   pool.query('SELECT * from construction_sites', (err, results) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    return res.send(results.rows);
-  }); */
   const sites = await Construction_site.findAll();
   res.send(sites);
 });
 
 app.post('/api/v1/construction-sites', async (req, res) => {
   const { name, coords } = req.body;
-  /* pool.query(
-    'INSERT INTO construction_sites (name,coords) VALUES ($1, $2)',
-    [name, coords],
-    (err, results) => {
-      if (err) {
-        return res.status(500).send(err);
-      }
-      return res.send(results);
-    },
-  ); */
   const site = await Construction_site.create({ name, coords });
   res.send(site);
+});
+
+app.put('/api/v1/construction-sites/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, coords } = req.body;
+  await Construction_site.update({ name, coords }, { where: { id } });
+  const result = await Construction_site.findOne({ where: { id } });
+  res.send(result);
+});
+
+app.delete('/api/v1/construction-sites/:id', async (req, res) => {
+  const { id } = req.params;
+  await Construction_site.destroy({ where: { id } });
+  res.send(id);
 });
 
 app.listen(port, (err) => {
