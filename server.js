ae6598ecccbc2ae230f2ba4851217a9e488d1475
Rémy <Remypottierpro@gmail.com>
2020-01-08 13:35:25 +0100

    Add jwt auth with sequelize

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
+const auth = require('./authentication');
 
 const app = express();
 const port = 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const constructionSites = require('./routes/construction_Sites');
+const users = require('./routes/users');
 
-const { Construction_site } = require('./server/models');
+app.use('/api/v1/construction-sites', constructionSites);
+app.use('/api/v1/users', users);
 
-app.get('/api/v1/construction-sites', async (req, res) => {
-  const sites = await Construction_site.findAll();
-  res.send(sites);
-});
 
-app.post('/api/v1/construction-sites', async (req, res) => {
-  const { name, coords } = req.body;
-  console.log(coords);
-  const site = await Construction_site.create({ name, coords });
-  res.send(site);
-});
-
-app.put('/api/v1/construction-sites/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name, coords } = req.body;
-  await Construction_site.update({ name, coords }, { where: { id } });
-  const result = await Construction_site.findOne({ where: { id } });
-  res.send(result);
-});
-
-app.delete('/api/v1/construction-sites/:id', async (req, res) => {
-  const { id } = req.params;
-  await Construction_site.destroy({ where: { id } });
-  res.send(id);
+app.post('/api/v1/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const { token } = await auth.authenticate({ email, password });
+    res.send(token);
+  } catch (err) {
+    res.status(401).send(err);
+  }
 });
 
 app.listen(port, (err) => {
