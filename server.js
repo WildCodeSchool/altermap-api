6d608f80a4b0ba2263e5e14f003d8553212d46f2
TanguyDuponchelle <tanguy.duponchelle@hotmail.fr>
2020-01-08 11:33:25 +0100

    add fields to table Construction_sites

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
 require('dotenv').config();
-
 const express = require('express');
 
 const app = express();
 const port = 4000;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 const { Construction_site } = require('./server/models');
 
 app.get('/api/v1/construction-sites', async (req, res) => {
-  const sites = await Construction_site.findAll();
-  res.send(sites);
+    const sites = await Construction_site.findAll();
+    res.send(sites);
 });
-
 app.post('/api/v1/construction-sites', async (req, res) => {
-  const { name, coords } = req.body;
-  const site = await Construction_site.create({ name, coords });
-  res.send(site);
+    console.log(req.body);
+    const { name, coords } = req.body;
+    const site = await Construction_site.create({ name, coords });
+    res.send(site);
 });
-
 app.put('/api/v1/construction-sites/:id', async (req, res) => {
-  const { id } = req.params;
-  const { name, coords } = req.body;
-  await Construction_site.update({ name, coords }, { where: { id } });
-  const result = await Construction_site.findOne({ where: { id } });
-  res.send(result);
+    const { id } = req.params;
+    const { name, coords } = req.body;
+    await Construction_site.update({ name, coords }, { where: { id } });
+    const result = await Construction_site.findOne({ where: { id } });
+    res.send(result);
 });
-
 app.delete('/api/v1/construction-sites/:id', async (req, res) => {
-  const { id } = req.params;
-  await Construction_site.destroy({ where: { id } });
-  res.send(id);
+    const { id } = req.params;
+    await Construction_site.destroy({ where: { id } });
+    res.send(id);
 });
-
 app.listen(port, (err) => {
-  if (err) {
-    throw new Error('Something bad happened...');
-  }
-  // eslint-disable-next-line no-console
-  console.log(`Server is listening on ${port}`);
+    if (err) {
+        throw new Error('Something bad happened...');
+    }
+    // eslint-disable-next-line no-console
+    console.log(`Server is listening on ${port}`);
 });
--- server/models/construction_site.js
+++ server/models/construction_site.js
@@ -1,10 +1,65 @@
-'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const Construction_site = sequelize.define('Construction_site', {
     name: DataTypes.STRING,
-    coords: DataTypes.DECIMAL
+    coords: DataTypes.INTEGER,
+    status: {
+      type: DataTypes.STRING,
+    },
+    year: {
+      type: DataTypes.STRING,
+    },
+    buyer: {
+      type: DataTypes.STRING,
+    },
+    contact: {
+      type: DataTypes.STRING,
+    },
+    address: {
+      type: DataTypes.STRING,
+    },
+    num_conv: {
+      type: DataTypes.STRING,
+    },
+    date_sign: {
+      type: DataTypes.STRING,
+    },
+    tonnage: {
+      type: DataTypes.STRING,
+    },
+    type_grave: {
+      type: DataTypes.STRING,
+    },
+    type_usage: {
+      type: DataTypes.STRING,
+    },
+    departement: {
+      type: DataTypes.STRING,
+    },
+    project_manager: {
+      type: DataTypes.TEXT,
+    },
+    commentary: {
+      type: DataTypes.TEXT,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+    },
+    photo: {
+      type: DataTypes.STRING,
+    },
+    tonnage_CUB: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    lots: {
+      type: DataTypes.STRING,
+    },
   }, {});
-  Construction_site.associate = function(models) {
+  Construction_site.associate = function (models) {
     // associations can be defined here
   };
   return Construction_site;
