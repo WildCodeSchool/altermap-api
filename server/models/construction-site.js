6ad4ed0f7abfb1ac284debf0e4f6d4fbce3cc422
TanguyDuponchelle <tanguy.duponchelle@hotmail.fr>
2020-01-09 13:17:57 +0100

    renamed models

--- server/models/construction-site.js
+++ server/models/construction-site.js
@@ -1,8 +1,62 @@
-
 module.exports = (sequelize, DataTypes) => {
   const ConstructionSite = sequelize.define('ConstructionSite', {
     name: DataTypes.STRING,
     coords: DataTypes.ARRAY(DataTypes.ARRAY(DataTypes.DECIMAL)),
+    status: {
+      type: DataTypes.STRING,
+    },
+    year: {
+      type: DataTypes.STRING,
+    },
+    buyer: {
+      type: DataTypes.STRING,
+    },
+    contact: {
+      type: DataTypes.STRING,
+    },
+    address: {
+      type: DataTypes.STRING,
+    },
+    num_conv: {
+      type: DataTypes.STRING,
+    },
+    date_sign: {
+      type: DataTypes.STRING,
+    },
+    tonnage: {
+      type: DataTypes.STRING,
+    },
+    type_grave: {
+      type: DataTypes.STRING,
+    },
+    type_usage: {
+      type: DataTypes.STRING,
+    },
+    departement: {
+      type: DataTypes.STRING,
+    },
+    project_manager: {
+      type: DataTypes.TEXT,
+    },
+    commentary: {
+      type: DataTypes.TEXT,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+    },
+    photo: {
+      type: DataTypes.STRING,
+    },
+    tonnage_CUB: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    lots: {
+      type: DataTypes.STRING,
+    },
   }, {});
   ConstructionSite.associate = (models) => {
     // associations can be defined here
