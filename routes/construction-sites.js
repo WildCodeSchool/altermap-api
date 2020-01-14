d732cfef7ecd035553a01172229c4c18c9e6579d
TanguyDuponchelle <tanguy.duponchelle@hotmail.fr>
2020-01-14 16:56:41 +0100

    fix fields on put endpoint

--- routes/construction-sites.js
+++ routes/construction-sites.js
@@ -28,8 +28,12 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
-  const { name, coords } = req.body;
-  await ConstructionSite.update({ name, coords }, { where: { id } });
+  const {
+    name, coords, status, buyer, contact, num_conv, date_sign, type_grave, year, type_usage, departement, project_manager, commentary, area, photo, lots, tonnage,
+  } = req.body;
+  await ConstructionSite.update({
+    name, coords, status, buyer, contact, num_conv, date_sign, type_grave, year, type_usage, departement, project_manager, commentary, area, photo, lots, tonnage,
+  }, { where: { id } });
   const result = await ConstructionSite.findOne({ where: { id } });
   res.send(result);
 });
