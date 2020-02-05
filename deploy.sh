f5ee8a2a25b6347d96b115ac4668df31ad453014
Rémy <Remypottierpro@gmail.com>
2020-02-05 16:58:32 +0100

    Remove seed in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,4 @@ cd /var/www/altermap-back
 git pull
 npm install --only=production
 npm run sequelize db:migrate
-npm run sequelize db:seed -- --seed 20200205144801-createSuperUser.js
 pm2 start npm -- start
